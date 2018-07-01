7af4c3fd69df61d0bfdfdb4013e54d52ad08e5e0
TJ Liwanag <timothyjliwanag@gmail.com>
2018-07-01 19:08:32 -0400

    added redux to manage state

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
+import { connect} from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
+import { setSearchField } from '../actions'
+
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchField
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)) 
+  }
+}
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      animals: [],
-      searchfield: ''
+      animals: []
     }
   }
 
@@ -20,20 +33,16 @@ class App extends Component {
       .then(users => this.setState({animals: users}));
   }
 
-
-  onSearchChange = (event) => {
-    this.setState({searchfield: event.target.value })
-  }
-
   render() {
-    const {animals, searchfield} = this.state;
+    const { animals } = this.state;
+    const { searchField, onSearchChange } = this.props;
     const filteredAnimals = animals.filter(animal =>{
-      return animal.name.toLowerCase().includes(searchfield.toLowerCase())
+      return animal.name.toLowerCase().includes(searchField.toLowerCase())
     })
     return (
       <div className='tc'>
         <h1 className='f1'>AsianFriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
+        <SearchBox searchChange={onSearchChange}/>
         <Scroll>
           <ErrorBoundry>
             <CardList animals={filteredAnimals} />
@@ -44,4 +53,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore} from 'redux';
 import App from  './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import { searchAnimals } from './reducers';
 import 'tachyons';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(searchAnimals)
+
+ReactDOM.render(
+                <Provider store={store}>
+                  <App  />
+                </Provider>, document.getElementById('root'));  
 registerServiceWorker();
